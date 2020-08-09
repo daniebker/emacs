e9dd4e494406b742e7cc88b12afd094497d61fc8
Daniel Baker <bakerdude@gmail.com>
2020-08-09 08:39:38 +0200

    autosave to tmp

--- customizations/misc.el
+++ customizations/misc.el
@@ -10,3 +10,14 @@
 
 ;; Go straight to scratch buffer on startup
 (setq inhibit-startup-message t)
+
+;; Put Auto save files in a tmp dir
+;; Save all tempfiles in $TMPDIR/emacs$UID/
+;; https://www.emacswiki.org/emacs/AutoSave
+(defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
+(setq backup-directory-alist
+    `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+    `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+    emacs-tmp-dir)
